f1d36fc7dbf49d48e8069c4a802d02f0830e0e19
batt1ebear <batt1ebear@foxmail.com>
2019-12-13 19:35:23 +0800

    Update rp.py

--- request_page/rp.py
+++ request_page/rp.py
@@ -5,18 +5,38 @@
 #假设每个块运行时间相同
 import time as t
 import random
+
 form=[[-1,0,0,0,0,0],[-1,1,0,0,0,100],[-1,2,0,0,0,200],[-1,3,0,0,0,300],[-1,4,0,0,0,400],[-1,5,0,0,0,500],[-1,6,0,0,0,600],[-1,7,0,0,0,700],[-1,8,0,0,0,800],[-1,9,0,0,0,900]]
 uid_map={}#给程序一个名字 与物理块号对应
 temPageId=[]#页表
 PageID=0#内存页id计数器
 missCount=0
 
+class FIFO:
+    def __init__(self):
+        self.stack=[]
+    
+    def load(self,item):
+        self.stack.append(item)
+    
+    def out(self):
+        re=self.stack[0]
+        del self.stack[0]
+        return re
+    
+    def refresh(self,item):
+        for i in range(self.stack.index(item),len(self.stack)-1):
+            self.stack[i]=self.stack[i+1]
+        self.stack[-1]=item
+
+stack=FIFO()
+
 for i in range(len(form)):
         dkey=chr(ord('a')+i)
         uid_map[(dkey)]=form[i]
 
 def show():
-    #TODO:传入调度方向 外存与内存状态图
+    #TODO:传入调度方向 外存与内存状态图d
     bar='*'*50
     print(bar)
     for i in range(3):
@@ -29,7 +49,8 @@ def show():
             print("          空")
             print('*'+' '*48+'*')
         except TypeError:
-            print("fuck")
+            #print("fuck")
+            pass
         for line in range(5):
             print('*'+' '*48+'*')
         print(bar)
@@ -41,6 +62,8 @@ def show():
 def run(order):
     global PageID
     global temPageId
+    global missCount
+    global stack
     ######记得简化一下 页表
     for num in range(len(form)):
         if form[num][0]!=-1:
@@ -48,20 +71,23 @@ def run(order):
     ########简化
     for i in range(len(order)):
         if order[i] in temPageId:
-            form[4]=1
+            form[order[i]][3]+=1
+            form[order[i]][4]=1
             print("%d未缺页"%order[i])
+            #实际这里应该检查一下修改位 看看是否要复制回去
             show()
 
         else:#不在页内
             if(len(temPageId)<3):#有空间
                 temPageId.append(order[i])
                 form[order[i]][0]=PageID
+                stack.load(PageID)
                 PageID+=1
                 form[order[i]][2]=1
                 print("%d缺页-直接调入"%order[i])
                 show()
-            else:#没空间 调出算法 这里就调出随机一个了 其实应该写一个FIFO
-                inde=temPageId[2]
+            else:#没空间 调出算法写一个FIFO
+                inde=stack.out()
                 form[inde][0]=-1
                 form[inde][2]=0
                 del temPageId[2]
@@ -69,13 +95,12 @@ def run(order):
 
                 temPageId.append(order[i])
                 form[order[i]][0]=PageID
+                stack.load(PageID)
                 PageID+=1
-                form[order[i]][2]=1
+                form[order[i]][2]=1 
                 print("%d缺页-调出%d后调入"%(order[i],form[inde][1]))
                 show()
-
-
-
+            missCount+=1
 
 
 def main():
